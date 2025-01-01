94dae5398ac6321a9b80210810cfe5aef98b051b
flaviano-faria <flsilv89@yahoo.com.br>
2025-01-01 20:53:55 -0300

    Organize imports

--- src/test/java/com/service/OrderServiceTest.java
+++ src/test/java/com/service/OrderServiceTest.java
@@ -3,14 +3,10 @@ package com.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 
 public class OrderServiceTest {
